9ee19caabd7263dbf2a83e172ed785134cd8db53
Salih <salihmorina555@gmail.com>
2025-10-06 20:31:49 +0200

    explorar datan och tar bort null värden på allt förutom Trip_Price och tar även bort outliers

--- src/taxipred/utils/constants.py
+++ src/taxipred/utils/constants.py
@@ -1,5 +1,6 @@
 from importlib.resources import files
+from pathlib import Path
+
+#DATA_PATH = Path(__file__).parents[1] / "data" / "taci_trip_pricing.csv"
 
 TAXI_CSV_PATH = files("taxipred").joinpath("data/taxi_trip_pricing.csv")
-
-# DATA_PATH = Path(__file__).parents[1] / "data"
