dcc78f34841eece2ce1cb010db85064639bff77a
Salih <salihmorina555@gmail.com>
2025-10-07 20:08:57 +0200

    skapar post predict

--- src/taxipred/backend/api.py
+++ src/taxipred/backend/api.py
@@ -1,10 +1,29 @@
+import joblib
 from fastapi import FastAPI
-from taxipred.backend.data_processing import TaxiData
+from pathlib import Path
+from taxipred.backend.data_processing import TaxiData, PredictionInput, preprocess_input 
+
+
+MODEL_PATH = Path(__file__).resolve().parent.parent / "models" / "gbr_final_model.joblib"
+
+
+MODEL = joblib.load(MODEL_PATH) 
+   
 
 app = FastAPI()
-
 taxi_data = TaxiData()
 
 @app.get("/taxi/")
 async def read_taxi_data():
     return taxi_data.to_json()
+
+@app.post("/predict")
+async def predict_price(input_data: PredictionInput):
+    
+    processed_df = preprocess_input(input_data)
+        
+    prediction = MODEL.predict(processed_df)[0]
+        
+    return {"predicted_price": round(float(prediction), 2)}
+        
+   
